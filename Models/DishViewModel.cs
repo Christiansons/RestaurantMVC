592c5a484a84d53dd1e7d3351fa633224781d130
Alex <alexander.christianson@outlook.com>
2024-09-27 21:21:05 +0200

    created admin and dish viewmodels

---
+++ Models/DishViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantMVC.Models
+{
+    public class DishViewModel
+    {
+        public string dishName { get; set; }
+        public int priceInSek { get; set; }
+        public bool isAvailable { get; set; }
+    }
+}
