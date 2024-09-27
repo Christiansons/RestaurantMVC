debc241256ea2502d7200218e7c10b18cc6385c5
Alex <alexander.christianson@outlook.com>
2024-09-27 21:19:50 +0200

    Added admin controlelr

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly string baseUrl = "http://localhost:5107/api/";
+        private readonly HttpClient _client;
+
+        public AdminController()
+        {
+            _client = new HttpClient();
+        }
+
+        public IActionResult AdminLogIn()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
