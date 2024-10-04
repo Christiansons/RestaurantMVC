95f6893ab00449a31fb815dce6f788843c3f5b75
Alex <alexander.christianson@outlook.com>
2024-10-04 14:26:24 +0200

    Added delete and edit actions for dishes

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RestaurantMVC.Models;
+using RestaurantMVC.Controllers;
+using System.Text;
 
 namespace RestaurantMVC.Controllers
 {
@@ -25,7 +27,7 @@ namespace RestaurantMVC.Controllers
 			return View(availableMenu);
 		}
 
-		public async Task<IActionResult> AdminMenuHandler()
+		public async Task<IActionResult> AdminDishHandler()
 		{
             var response = await _client.GetAsync(baseUrl);
             var json = await response.Content.ReadAsStringAsync();
@@ -33,7 +35,7 @@ namespace RestaurantMVC.Controllers
 
 			if(!response.IsSuccessStatusCode)
 			{
-				
+				return RedirectToAction("Error", "HomeController");
 			}
 
             return View(menu);
@@ -41,7 +43,44 @@ namespace RestaurantMVC.Controllers
 
 		public async Task<IActionResult> UpdateDish(int id)
 		{
+			var response = await _client.GetAsync($"{baseUrl}/{id}");
+			var json = await response.Content.ReadAsStringAsync();
+			var dish = JsonConvert.DeserializeObject<DishViewModel>(json);
+			return View(dish);
+		}
 
+		[HttpPost]
+		public async Task<IActionResult> UpdateDish(DishViewModel dish)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View(dish);
+			}
+
+			var json = JsonConvert.SerializeObject(dish);
+			var content = new StringContent(json, Encoding.UTF8, "application/json");
+			var response = await _client.PutAsync($"{baseUrl}/update/{dish.id}", content);
+
+			if(!response.IsSuccessStatusCode)
+			{
+				return View(dish);
+			}
+
+			return RedirectToAction("AdminDishHandler");
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteDish(int id)
+		{
+			var response = await _client.DeleteAsync($"{baseUrl}/delete/{id}");
+
+			if (!response.IsSuccessStatusCode)
+			{
+				return View();
+			}
+
+			return RedirectToAction("AdminDishHandler");
 		}
 	}
 }
