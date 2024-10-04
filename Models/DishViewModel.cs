552ad44d989be7f531336b9f7ac7df0f5e5237f5
Alex <alexander.christianson@outlook.com>
2024-10-04 14:26:40 +0200

    Added id to view model

--- Models/DishViewModel.cs
+++ Models/DishViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class DishViewModel
     {
+        public int id { get; set; }
         public string dishName { get; set; }
         public int priceInSek { get; set; }
         public bool isAvailable { get; set; }
